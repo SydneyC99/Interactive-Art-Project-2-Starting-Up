b9c0b5826f9f2800251bf870e6cf730627a7df30
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-11-01 10:28:55 -0400

    Maze Tiles Functional
    Co-authored-by: Rob Duarte <me@robduarte.com>

--- maze.js
+++ maze.js
@@ -1,20 +1,54 @@
-let sprite;
+let char;
+let charImg, tileImg;
 let canvasHeight;
 let canvasWidth;
+
+function preload() {
+    charImg = loadImage('Assets/Character Sprite.png');
+    tileImg = loadImage('Assets/TileSheet64x64.png');
+}
+
 function setup() {
 
-    // canvasHeight = 200;
-    // canvasWidth = 200;
+    canvasHeight = 240;
+    canvasWidth = 240;
     
-    // let canvas = new Canvas(canvasWidth, canvasHeight, 'pixelated x2');
-    // background('black');
-
+    new Canvas(canvasWidth, canvasHeight, 'pixelated x2');
+    allSprites.pixelPerfect = true;
+    allSprites.rotationLock = true;
+    allSprites.tileSize = 16;
     
-    // sprite = new Sprite();
+    // char = new Sprite(1.5,1.5, 16,16);
+    // char.spriteSheet = charImg;
+    // char.addAnis({
+    //     front: {row: 0, col: 0}
+    // })
+    let pathCornerTL = new Group();
+    pathCornerTL.collider = 'none';
+    pathCornerTL.spriteSheet = tileImg;
+    pathCornerTL.addAni({w:1, h:1, row: 0, col: 0});
+    pathCornerTL.tile = 'c';
     
+    let pathTop = new Group();
+    pathTop.collider = 'none';
+    pathTop.spriteSheet = tileImg;
+    pathTop.addAni({w:1, h:1, row: 0, col: 1});
+    pathTop.tile = 't';
 
+    let pathLeft = new Group();
+    pathLeft.collider = 'none';
+    pathLeft.spriteSheet = tileImg;
+    pathLeft.addAni({w:1, h:1, row: 1, col: 0});
+    pathLeft.tile = 'l';
 
-    textFont("Courier", 24);
+    new Tiles(
+        [
+            'ctttttttttttttc',
+            'l.............l'
+        ],
+        0.5, 0.5, 1, 1
+    );
+
 }
 
 function draw() {
